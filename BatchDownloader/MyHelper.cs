a4de4335cdbd3aa586dba6d3000cd5ce4c09c419
ls121ls <314888192@qq.com>
2016-05-16 11:01:47 +0800

    增加了帮助类

---
+++ BatchDownloader/MyHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BatchDownloader
+{
+    public class MyHelper
+    {
+        /// <summary>
+        /// 去除爬虫出来的错误链接
+        /// </summary>
+        public void T()
+        {
+            string[] strs = File.ReadAllLines("F://1/1.txt");
+            List<string> stemps = new List<string>();
+            foreach (var str in strs)
+            {
+                if (!String.IsNullOrEmpty(str))
+                {
+                    string temp = str.Split('-')[3];
+                    if (!String.IsNullOrEmpty(temp))
+                    {
+                        stemps.Add(str);
+                    }
+                }
+            }
+            string path = "F://1/2.txt";
+            File.WriteAllLines(path, stemps);
+            Console.ReadKey();
+        }
+    }
+}
