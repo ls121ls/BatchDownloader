21d09edde077217c107e0914e438b443a56fd9ab
ls121ls <314888192@qq.com>
2017-01-22 09:14:07 +0800

    提取一些公共方法

--- BatchDownloader/Program2.cs
+++ BatchDownloader/Program2.cs
@@ -17,7 +17,7 @@ namespace BatchDownloader
     class Program2
     {
         //起始页
-        static int strartIndex = 1;
+        static int strartIndex = 2;
         //终止页
         static int endIndex = 50000;
 
@@ -26,7 +26,7 @@ namespace BatchDownloader
         //文件最小值
         static int minLength = 1;
         //线程数
-        private static int count = 1;
+        private static int count = 10;
 
         static MyDelegate myDelegate = new MyDelegate(DownloadFile);
         //当前下载最大Id
@@ -46,58 +46,39 @@ namespace BatchDownloader
             Console.ReadKey();
         }
         private static Object thisLock = new Object();
-        private static Object thisLock1 = new Object();
         private static Object thisLock2 = new Object();
         public static bool DownloadFile(object id)
         {
             string str = "";
             str = "http://www.11bz.org/modules/article/txtarticle.php?id=" + id;
+            Helper helper = new Helper();
             try
             {
-                Log("正在下载" + id);
+                //Log("正在下载" + id);
                 WebRequest request = WebRequest.Create(str);
                 var temp=request.GetResponse().Headers;
                 if (temp.GetValues("content-disposition")==null)
                 {
-                    Log(id + "没有文件2");
+                    helper.Log(id + "没有文件2");
                     return true;
                 }
-                string fileName = GetFileName(Encoding.GetEncoding("gbk").GetString(Encoding.GetEncoding("ISO-8859-1").GetBytes(temp.GetValues("content-disposition").FirstOrDefault())));
-
-                StreamToFile(request.GetResponse().GetResponseStream(), "F://1/" + fileName);
+                string fileName = helper.GetFileName(Encoding.GetEncoding("gbk").GetString(Encoding.GetEncoding("ISO-8859-1").GetBytes(temp.GetValues("content-disposition").FirstOrDefault())));
+                helper.StreamToFile(request.GetResponse().GetResponseStream(), "F://1/" + fileName);
                 lock (thisLock)
                 {
                     File.AppendAllText("F://1/111下载.txt", id + "\r\n");
                 }
-                Log(id + "下载完成");
+                //Log(id + "下载完成");
                 return true;
             }
             catch (Exception e)
             {
-                Log(id + "下载异常，重新下载");
+                helper.Log(id + "下载异常，重新下载");
                 myDelegate.BeginInvoke(id, Completed, null);
                 return false;
             }
         }
-        /// <summary>   
-        /// 将 Stream 写入文件   
-        /// </summary>   
-        public static void StreamToFile(Stream responseStream, string fileName)
-        {
-            //创建本地文件写入流
-            Stream stream = new FileStream(fileName, FileMode.Create);
-
-            byte[] bArr = new byte[1024];
-            int size = responseStream.Read(bArr, 0, (int)bArr.Length);
-            while (size > 0)
-            {
-                stream.Write(bArr, 0, size);
-                size = responseStream.Read(bArr, 0, (int)bArr.Length);
-            }
-            stream.Close();
-            responseStream.Close();
-
-        } 
+        
 
         static void Completed(IAsyncResult result)
         {//获取委托对象，调用EndInvoke方法获取运行结果
@@ -118,7 +99,32 @@ namespace BatchDownloader
             }
         }
 
-        static void Log(string str)
+    }
+
+    public class Helper
+    {
+        /// <summary>   
+        /// 将 Stream 写入文件   
+        /// </summary>   
+        public void StreamToFile(Stream responseStream, string fileName)
+        {
+            //创建本地文件写入流
+            Console.Write(fileName+"正在下载\n");
+            Stream stream = new FileStream(fileName, FileMode.Create);
+
+            byte[] bArr = new byte[1024];
+            int size = responseStream.Read(bArr, 0, (int)bArr.Length);
+            while (size > 0)
+            {
+                stream.Write(bArr, 0, size);
+                size = responseStream.Read(bArr, 0, (int)bArr.Length);
+            }
+            stream.Close();
+            responseStream.Close();
+            Console.Write(fileName + "下载完成\n");
+        }
+        private static Object thisLock1 = new Object();
+        public  void Log(string str)
         {
             Console.WriteLine(str);
             lock (thisLock1)
@@ -127,8 +133,7 @@ namespace BatchDownloader
             }
         }
 
-
-        static string GetFileName(string str)
+        public string GetFileName(string str)
         {
             return Regex.Replace(str, ".*filename=", "");
         }
